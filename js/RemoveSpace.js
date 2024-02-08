9ba430089c41b00925561fa093ae86c4268e81cb
Abu10thahir7-github <142816135+Abu10thahir7-github@users.noreply.github.com>
2024-02-08 09:49:14 +0530

    add pro

---
+++ js/RemoveSpace.js
@@ -0,0 +1,5 @@
+
+const string = '  Hello,  World!  ,   How are you?    1  2  3';
+const result = string.replace(/\s/g, '');
+
+console.log(result);
---
+++ js/countWord.js
@@ -0,0 +1,6 @@
+
+const sentence = 'This is a sample sentence';
+const words = sentence.split(' ');
+const wordCount = words.length;
+
+console.log(wordCount);
